1eadfa1be158fc1f431bda7ed272fe82e33a75bd
Austin Johnson <austintjohnson92@gmail.com>
2022-06-09 12:49:50 -0700

    wip

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send(`<h1>${process.env.MESSAGE}</h1>`)
+})
+
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = server
